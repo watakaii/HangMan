7d21e958c58ed48e462cd190c86832acdcda3983
watakai <nikitapashin3@gmail.com>
2024-07-16 14:27:46 +0300

    Check for already inputted error letters + error letters visualisation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
-
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
+        startGameMenu();
+    }
+
+    private static void startGameMenu(){
         System.out.println("Введите (1) чтобы начать игру, (2) чтобы закрыть приложение");
         String menuInput = in.next();
         while (!Objects.equals(menuInput, "1") && !Objects.equals(menuInput, "2")){
@@ -23,13 +24,14 @@ public class Main {
     private static void startGame() {
         String word;
         int miss_count = 0;
+        List<Character> error_chars = new ArrayList();
         int wordId = getRandomNumber();
         word = WORDS[wordId];
         char[] mask = new char[word.length()];
         for (int i = 0; i < word.length(); i++) {
             mask[i] = '_';
         }
-        startGameLoop(word,mask, miss_count);
+        startGameLoop(word,mask, miss_count, error_chars);
     }
 
     public static ArrayList<Integer> findPositions(String string, char character) {
@@ -51,12 +53,13 @@ public class Main {
     private static String[] WORDS = new String[] {"виселица", "шаловливость", "кормилица", "яркость", "прозорливость"};
 
 
-    public static void startGameLoop(String word, char[] mask, int miss_count) {
-        playerTurn(word, mask, miss_count);
+    public static void startGameLoop(String word, char[] mask, int miss_count, List<Character> error_chars) {
+        playerTurn(word, mask, miss_count, error_chars);
     }
 
-    public static void playerTurn(String word, char[] mask, int miss_count) {
+    public static void playerTurn(String word, char[] mask, int miss_count, List<Character> error_chars) {
         System.out.println(miss_count);
+        System.out.println(error_chars);
         System.out.println(mask);
         String playerInput = in.next();
         String isNumber = ".*\\d.*";
@@ -66,10 +69,10 @@ public class Main {
             playerInput = in.next();
         }
         char playerChoice = playerInput.charAt(0);
-        letterCheck(playerChoice, word, mask, miss_count);
+        letterCheck(playerChoice, word, mask, miss_count, error_chars);
     }
 
-    public static void letterCheck(char playerChoice, String word, char[] mask, int miss_count){
+    public static void letterCheck(char playerChoice, String word, char[] mask, int miss_count, List<Character> error_chars){
         if(word.contains(String.valueOf(playerChoice))) {
             ArrayList<Integer> result = findPositions(word, playerChoice);
             for (Integer n : result){
@@ -77,22 +80,30 @@ public class Main {
             }
         }
         else {
-            miss_count += 1;
+            if (error_chars.contains(playerChoice)) {
+                System.out.println("Вы уже вводили такую букву");
+                checkGameState(word, mask, miss_count, error_chars);
+            }
+            else {
+                System.out.println("Такой буквы нет");
+                error_chars.add(playerChoice);
+                miss_count += 1;
+            }
         }
-        checkGameState(word, mask, miss_count);
+        checkGameState(word, mask, miss_count, error_chars);
     }
 
-    private static void checkGameState(String word, char[] mask, int missCount) {
+    private static void checkGameState(String word, char[] mask, int missCount, List<Character> error_chars) {
         int FATAL_MISS_COUNT = 6;
         if (!(new String(mask).contains("_"))){
             System.out.println("YOU WIN");
-            System.exit(0);
+            startGameMenu();
         }
         else if (missCount == FATAL_MISS_COUNT){
             System.out.println("YOU LOSE");
-            System.exit(0);
+            startGameMenu();
         }
-        playerTurn(word, mask, missCount);
+        playerTurn(word, mask, missCount, error_chars);
     }
 
 }
